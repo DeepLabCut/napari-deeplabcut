2ade7f50c7e232f2dfd2f9e8bedc9c7ed8e22f55
Jessy Lauer <30733203+jeylau@users.noreply.github.com>
2022-06-03 20:03:59 +0200

    Delete outdated test scripts

--- src/napari_deeplabcut/_tests/test_sample_data.py
+++
@@ -1,7 +0,0 @@
-# from napari_deeplabcut import make_sample_data
-
-# add your tests here...
-
-
-def test_something():
-    pass
--- src/napari_deeplabcut/_tests/test_writer.py
+++
@@ -1,7 +0,0 @@
-# from napari_deeplabcut import write_single_image, write_multiple
-
-# add your tests here...
-
-
-def test_something():
-    pass
