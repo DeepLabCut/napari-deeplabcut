2b03ca5e761f22a9953573ffaf93ebb24f18b414
Jessy Lauer <30733203+jeylau@users.noreply.github.com>
2023-08-30 09:31:29 +0200

    Replace QLabels in ColorSchemeDisplay with ClickableLabels

--- src/napari_deeplabcut/_widgets.py
+++ src/napari_deeplabcut/_widgets.py
@@ -18,7 +18,7 @@ from napari.layers.utils.layer_utils import _features_to_properties
 from napari.utils.events import Event
 from napari.utils.history import get_save_history, update_save_history
 from qtpy.QtCore import Qt, QTimer, Signal, QSize, QPoint, QSettings
-from qtpy.QtGui import QPainter, QIcon, QAction
+from qtpy.QtGui import QPainter, QIcon, QAction, QCursor
 from qtpy.QtWidgets import (
     QButtonGroup,
     QCheckBox,
@@ -989,6 +989,26 @@ class QtWelcomeWidget(QWidget):
         self.sig_dropped.emit(event)
 
 
+class ClickableLabel(QLabel):
+    clicked = Signal()
+
+    def __init__(self, text="", color="turquoise", parent=None):
+        super().__init__(text, parent)
+        self._default_style = self.styleSheet()
+        self.color = color
+
+    def mousePressEvent(self, event):
+        self.clicked.emit()
+
+    def enterEvent(self, event):
+        self.setCursor(QCursor(Qt.PointingHandCursor))
+        self.setStyleSheet(f"color: {self.color}")
+
+    def leaveEvent(self, event):
+        self.unsetCursor()
+        self.setStyleSheet(self._default_style)
+
+
 class LabelPair(QWidget):
     def __init__(self, color: str, name: str, parent: QWidget):
         super().__init__(parent)
@@ -997,7 +1017,7 @@ class LabelPair(QWidget):
         self._part_name = name
 
         self.color_label = QLabel("", parent=self)
-        self.part_label = QLabel(name, parent=self)
+        self.part_label = ClickableLabel(name, parent=self)
 
         self.color_label.setToolTip(name)
         self.part_label.setToolTip(name)
