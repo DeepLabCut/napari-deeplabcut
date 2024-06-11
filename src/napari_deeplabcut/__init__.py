b5fc31dc0fa03e2b7bdcd01b9592c5c0b0c3a670
Jessy Lauer <30733203+jeylau@users.noreply.github.com>
2024-06-11 11:44:19 +0200

    Silence vispy warnings (#132)

--- src/napari_deeplabcut/__init__.py
+++ src/napari_deeplabcut/__init__.py
@@ -1,7 +1,21 @@
+import logging
 import warnings
 
 # FIXME: Circumvent the need to access window.qt_viewer
 warnings.filterwarnings("ignore", category=FutureWarning)
+
+
+class VispyWarningFilter(logging.Filter):
+    def filter(self, record):
+        ignore_messages = [
+            "delivering touch release to same window QWindow(0x0) not QWidgetWindow",
+            "skipping QEventPoint",
+        ]
+        return not any(msg in record.getMessage() for msg in ignore_messages)
+
+
+vispy_logger = logging.getLogger("vispy")
+vispy_logger.addFilter(VispyWarningFilter())
 
 try:
     from ._version import version as __version__
