b53704bf51b8fc862c7824ca4ae8472379a3cd3c
C-Achard <cyril.achard@epfl.ch>
2026-09-11 18:41:17 +0200

    Avoid private napari keybinding imports
    Replace `increment_dims_left/right` imports from napari internals with local helpers built on the public `Viewer.dims` API, preventing plugin import failures when napari private modules move. Also narrow `napari_compat` re-exports by removing `qt_dock_widgets` and `register_points_action` from `__all__`.

--- src/napari_deeplabcut/napari_compat/__init__.py
+++ src/napari_deeplabcut/napari_compat/__init__.py
@@ -9,16 +9,12 @@ from .points_layer import (
     apply_points_layer_ui_tweaks,
     install_add_wrapper,
     install_paste_patch,
-    register_points_action,
 )
 from .proxy import unwrap
-from .window import qt_dock_widgets
 
 __all__ = [
     "apply_points_layer_ui_tweaks",
     "install_add_wrapper",
     "install_paste_patch",
-    "qt_dock_widgets",
-    "register_points_action",
     "unwrap",
 ]
